8f98882d04bd0d4eb89bbff7e272a664a480c88d
Hideo Kojima <kostyaelizarov@mail.ru>
2020-07-31 17:13:44 +0300

    a

---
+++ React/01-first-project/react-project-1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://sk.ru/media/images/huawei-logo.original.original.png"></img>
+        </header>
+        )
+}
+
+export default Header
---
+++ React/01-first-project/react-project-1/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://sun9-32.userapi.com/c855232/v855232569/f38f/nG68QZr20xk.jpg"></img>
+                Main content
+            </div>
+        </div>
+    )
+}
+
+export default Profile

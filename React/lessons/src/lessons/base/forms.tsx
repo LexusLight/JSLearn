19a40a84dfc686e8610ada804357d4cc70938e77
lexuslight <kostyaelizarov@mail.ru>
2020-09-18 00:27:57 +0300

    Ещё формы!

--- React/lessons/src/lessons/base/forms.tsx
+++ React/lessons/src/lessons/base/forms.tsx
@@ -3,26 +3,87 @@ import style from './base.module.css'
 
 
 const Forms = (props:any) => {
-    let [value,setValue] = useState(0);
+    let [value,setName] = useState("name");
+    let [text,setTextArea] = useState("Вот так задаётся textarea");
+    let [select,setSelect] = useState("Ваш выбор")
 
-    const handleChange = (e:any) => {
-        setValue(e.target.value)
+    const handleChangeName = (e:any) => { //для имени
+        setName(e.target.value)
     }
-    const handleSubmit = (e:any) => {
+    const handleSubmitName = (e:any) => {
         alert('Отправленное имя: ' + value);
         e.preventDefault();
     }
 
+    const handleChangeText = (e:any) => { //для textarea
+        setTextArea(e.target.value)
+    }
+
+    const handleChangeSelect = (e:any) => { //для селектора
+        setSelect(e.target.value)
+    }
+
     return (
         <div className={style.center}>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmitName}>
                 <label>
                     Имя:
-                    <input type="text" value={value} onChange={handleChange} />        </label>
+                    <input type="text" value={value} onChange={handleChangeName} />
+                </label>
                 <input type="submit" value="Отправить" />
+            </form>
+
+            <br/>
+
+            <form>
+                <label>
+                    <textarea value={text} onChange={handleChangeText}/>
+                    <br/>
+                    <br/>
+                    <input value={select}/>
+                    {/*<select multiple={true} value={['Б', 'В']}> В атрибут value можно передать массив,
+                    что позволит выбрать несколько опций в теге select: */}
+                    <select value={select} onChange={handleChangeSelect}>
+                        <option value="grapefruit">Грейпфрут</option>
+                        <option value="lime">Лайм</option>
+                        <option value="coconut">Кокос</option>
+                        <option value="mango">Манго</option>
+                    </select>
+                    <br/>
+                    <br/>
+                    <input type="file" />
+                    <br/>
+                    <br/>
+                </label>
             </form>
         </div>
     );
 }
 
 export default  Forms;
+
+// class EssayForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {      value: 'Будьте любезны, напишите сочинение о вашем любимом DOM-элементе.'    };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//
+//     handleChange(event) {    this.setState({value: event.target.value});  }
+//     handleSubmit(event) {
+//         alert('Сочинение отправлено: ' + this.state.value);
+//         event.preventDefault();
+//     }
+//
+//     render() {
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>
+//                     Сочинение:
+//                     <textarea value={this.state.value} onChange={this.handleChange} />        </label>
+//                 <input type="submit" value="Отправить" />
+//             </form>
+//         );
+//     }
+// }
